819a02bcb7f9bbafded1da8166427e9deb6d34bd
Sakthivelamari <sakthivelmaribe@gmail.com>
2026-09-15 09:41:55 +0530

    Type conversion file added

---
+++ Java_Course/Type_conversion.java
@@ -0,0 +1,20 @@
+package Java_Course;
+
+public class Type_conversion {
+  public static  void main(String args[])
+  {
+    byte value=21;
+    int value2=value;
+    System.out.println(value2);
+    float f=5.6f;
+    int int_value=(int)f;
+    System.out.println((int_value));
+    int a=300;
+    byte b=(byte)a;
+    System.out.println(b);
+    float c=7.5f;
+    int d=(int)c;
+    System.out.println((d));
+  }
+  
+}
