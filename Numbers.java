118cef1689eb2b2a618beebaaa01454e701a563d
Sakthivelamari <sakthivelmaribe@gmail.com>
2026-09-15 09:23:01 +0530

    The Numbers formal file is going to add

---
+++ Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers{
+  public static void main(String args[])
+  {
+    int num1=0b101;
+    System.out.println(num1);
+    int num2=10_00_00_000;
+    System.out.println(num2);
+    char c='a';
+    c++;
+    System.out.println(c);
+  }
+}
